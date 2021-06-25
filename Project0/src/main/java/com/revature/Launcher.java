8e8d9d1afdd4950d391069f1fc6312714280380c
cbellaro <cbellaro@gmail.com>
2021-06-25 16:25:46 -0400

    sql database successfully connected

--- Project0/src/main/java/com/revature/Launcher.java
+++ Project0/src/main/java/com/revature/Launcher.java
@@ -1,9 +1,19 @@
 package com.revature;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.revature.utils.ConnectionUtil;
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		try(Connection conn = ConnectionUtil.getConnection()) {
+			System.out.println("connection successful");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
